9f4b5930e4d15ee03c641c72d62d66f090f8e94f
Kevin Botero <kbotero201@gmail.com>
2020-09-09 19:44:33 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,10 @@ def using_concat(array, element)
  array.concat(element)
 end
 
-def using_insert(array, element)
- array.insert(element)
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+  list_of_esoteric_programming_languages = ["Ante", "ArnoldC", "Befunge", "Binary lambda calculus", "Chef", "GolfScript", "Ook!"]
+  another_esoteric_language = "Malbolge"
+  new_array = list_of_esoteric_programming_languages.insert(4, another_esoteric_language)
 end
 
 def using_uniq(array)
